d86a7a30f817c8a44183699f0aca7e7fe22188ab
João Macêdo <joaomacedx@gmail.com>
2023-02-21 12:42:32 -0300

    feature: create statement/date method

--- src/index.js
+++ src/index.js
@@ -87,6 +87,20 @@ app.post("/withdraw", verifyIfExistsAccountCpf, (request, response)=>{
 
   return response.status(201).send();
  });
+
+ app.get("/statement/date", verifyIfExistsAccountCpf, (request, response)=>{
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + "00:00");
+   const statement = customer.statement.filter((statement)=> 
+    statement.created_at.toDateString()== 
+       new Date(dateFormat).toDateString()
+   );
+
+
+  return response.json(customer.statement);
+});
  
 app.listen(8080);
 
