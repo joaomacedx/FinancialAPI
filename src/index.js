5f11296c3fb91dd6e81b05c0d89301ad748bcf4c
João Macêdo <joaomacedx@gmail.com>
2023-02-21 18:32:49 -0300

    feature: create routes to manipulate account

--- src/index.js
+++ src/index.js
@@ -92,15 +92,35 @@ app.post("/withdraw", verifyIfExistsAccountCpf, (request, response)=>{
   const { customer } = request;
   const { date } = request.query;
 
-  const dateFormat = new Date(date + "00:00");
+  const dateFormat = new Date(date + " 00:00");
    const statement = customer.statement.filter((statement)=> 
     statement.created_at.toDateString()== 
        new Date(dateFormat).toDateString()
    );
-
-
-  return response.json(customer.statement);
+  return response.json(statement);
 });
- 
+
+app.put("/account", verifyIfExistsAccountCpf, (request, response)=>{
+  const { name } = request.body;
+  const { customer } = request;
+   
+  customer.name = name;
+
+  return response.status(201).send();
+});
+app.get("/account", verifyIfExistsAccountCpf, (request, response)=>{
+   const { customer } = request;
+
+   return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCpf, (request, response)=>{
+  const { customer }= request;
+
+  customer.splice(customer, 1);
+
+  return response.status(204);
+})
+
 app.listen(8080);
 
